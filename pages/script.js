05dffcdc1d21816647ace9af3ca52ae91d1e7a5f
Lucas Santiago <luccassantiago.contato@gmail.com>
2022-10-25 22:48:16 -0300

    Exercícios 04

---
+++ pages/script.js
@@ -0,0 +1,20 @@
+function exerc01(){
+    alert("Olá, mundo!")
+}
+
+function exerc02(){
+    alert("'script src='script.js'></script>'")
+}
+
+function exerc03(){
+    var valor1 = Number(prompt("Digite o primeiro número: "))
+    var valor2 = Number(prompt("Digite o segundo número: "))
+    var resultado = valor1 + valor2
+
+    alert("O resultado desta soma é: " + resultado)
+}
+
+function exerc04(){
+    var nome = prompt("Qualquer o seu nome completo?")
+    alert("Muito prazer, " + nome + "!")
+}
